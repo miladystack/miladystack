0f04691b20f3aff2cb0d8f77ce7dec563734467c
bugsmo <18550039021@163.com>
2025-12-03 11:34:24 +0800

    feat: authn

--- pkg/authn/authn.go
+++ pkg/authn/authn.go
@@ -1,8 +1,38 @@
 package authn
 
 import (
+	"context"
+
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
+
+// IToken defines methods to implement a generic token.
+type IToken interface {
+	// Get token string.
+	GetToken() string
+	// Get token type.
+	GetTokenType() string
+	// Get token expiration timestamp.
+	GetExpiresAt() int64
+	// JSON encoding
+	EncodeToJSON() ([]byte, error)
+}
+
+// Authenticator defines methods used for token processing.
+type Authenticator interface {
+	// Sign is used to generate a token.
+	Sign(ctx context.Context, userID string) (IToken, error)
+
+	// Destroy is used to destroy a token.
+	Destroy(ctx context.Context, accessToken string) error
+
+	// ParseClaims parse the token and return the claims.
+	ParseClaims(ctx context.Context, accessToken string) (*jwt.RegisteredClaims, error)
+
+	// Release used to release the requested resources.
+	Release() error
+}
 
 // Encrypt encrypts the plain text with bcrypt.
 func Encrypt(source string) (string, error) {
